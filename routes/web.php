77bf1d16aac19fe28fdbd2517826643df3b98352
wdEffort <yoman3011@gmail.com>
2022-02-22 18:08:18 +0900

    로그인/로그아웃 라우트 추가

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
 use Painkill2r\InflearnLectureLib\Routing\Route;
+use App\Middlewares\RequireMiddleware;
+use App\Middlewares\CsrfTokenMiddleware;
+use App\Middlewares\AuthMiddleware;
 
 Route::add("get", "/", "\App\Controllers\IndexController::index");
+
+Route::add("get", "/auth/login", "\App\Controllers\AuthController::showLoginForm");
+
+Route::add("post", "/auth", "\App\Controllers\AuthController::login", [RequireMiddleware::class, CsrfTokenMiddleware::class]);
+
+Route::add("post", "/auth/logout", "\App\Controllers\AuthController::logout");
