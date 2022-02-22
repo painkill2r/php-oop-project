225c6fdc189e20eec20c837de7852dc88f8a8b69
wdEffort <yoman3011@gmail.com>
2022-02-22 18:08:42 +0900

    로그인/로그아웃 처리 Controller 및 View 추가

---
+++ app/Services/AuthService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Services;
+
+use Painkill2r\InflearnLectureLib\Database\Adaptor;
+
+class AuthService
+{
+
+    public static function login($email, $password)
+    {
+        if ($user = current(Adaptor::getAll("SELECT * FROM users WHERE email = ?", [$email], \App\User::class))) {
+            if (\password_verify($password, $user->password)) {
+                return $_SERVER['user'] = $user;
+            }
+        }
+    }
+
+    public static function logout()
+    {
+        //session_unset();
+        return session_destroy();
+    }
+}
